ddd10cf91147ebf7c35391162c8fc197fa0f6300
JR_ <jrotl@hotmail.com>
2024-09-17 01:30:56 -0300

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LABEL=""
+
+BACKUP_FILE="$HOME/backup_${LABEL}_$(date +%Y%m%d%H%M).tar.gz"
+
+find $HOME -type f -not -path '*/.*' -mmin -5 -exec tar -czf "$BACKUP_FILE" {} +
+
+FTP_HOST=""
+FTP_USER=""
+FTP_PASS=""
+
+curl -T "$BACKUP_FILE" ftp://$FTP_USER:$FTP_PASS@$FTP_HOST/
+
+rm "$BACKUP_FILE"
