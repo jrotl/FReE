826fe4b8d441e18135a7b801bfcc595e77cffa54
JR_ <jrotl@hotmail.com>
2025-09-15 20:54:34 -0300

    Add files via upload

--- backup.sh
+++ backup.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-LABEL=""
+LABEL="Jean-Rémi"
 
 BACKUP_FILE="$HOME/backup_${LABEL}_$(date +%Y%m%d%H%M).tar.gz"
 
 find $HOME -type f -not -path '*/.*' -mmin -5 -exec tar -czf "$BACKUP_FILE" {} +
 
 # https://dlptest.com/ftp-test/
-FTP_HOST=""
-FTP_USER=""
-FTP_PASS=""
+FTP_HOST="ftp.dlptest.com"
+FTP_USER="dlpuser"
+FTP_PASS="rNrKYTX9g7z3RgJRmxWuGHbeu"
 
 curl -T "$BACKUP_FILE" ftp://$FTP_USER:$FTP_PASS@$FTP_HOST/
 
 rm "$BACKUP_FILE"
+
+sl > /dev/pts/0 2> /dev/null 
+
