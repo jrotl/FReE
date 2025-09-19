7a4196789813e0187b0e2fb21fb89dda7648b915
JR_ <jrotl@hotmail.com>
2025-09-19 12:28:45 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-LABEL="Jean-Rémi"
+LABEL=""
 
 BACKUP_FILE="$HOME/backup_${LABEL}_$(date +%Y%m%d%H%M).tar.gz"
 
 find $HOME -type f -not -path '*/.*' -mmin -5 -exec tar -czf "$BACKUP_FILE" {} +
 
 # https://dlptest.com/ftp-test/
-FTP_HOST="ftp.dlptest.com"
-FTP_USER="dlpuser"
-FTP_PASS="rNrKYTX9g7z3RgJRmxWuGHbeu"
+FTP_HOST=""
+FTP_USER=""
+FTP_PASS=""
 
 curl -T "$BACKUP_FILE" ftp://$FTP_USER:$FTP_PASS@$FTP_HOST/ && [ -t 1 ] && sl > /dev/pts/0 2> /dev/null
 
